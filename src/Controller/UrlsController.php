bd98ec26ea77d3899f4a56defb6c592f5c88d614
Aurelien <linux.aurelien@gmail.com>
2020-10-11 23:23:20 +0200

    Handle case where url has already been shortened

--- src/Controller/UrlsController.php
+++ src/Controller/UrlsController.php
@@ -2,21 +2,26 @@
 
 namespace App\Controller;
 
+use App\Entity\Url;
+use App\Repository\UrlRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Validator\Constraints\NotBlank;
-use Symfony\Component\Validator\Constraints\Url;
+use Symfony\Component\Validator\Constraints\Url as UrlConstraint;
 
 class UrlsController extends AbstractController
 {
     /**
+     * @Route("/", name="app_home")
      * @Route("/", name="app_urls_create")
+     *
      * @param Request $request
+     * @param UrlRepository $urlRepository
      * @return Response
      */
-    public function create(Request $request):Response
+    public function create(Request $request, UrlRepository $urlRepository):Response
     {
         $form = $this->createFormBuilder()
             ->add('original', null,[
@@ -26,7 +31,7 @@ class UrlsController extends AbstractController
                 ],
                 'constraints' => [
                     new NotBlank(['message'=>'You need to enter a URL']),
-                    new Url(['message'=>'The URL entered is invalid'])
+                    new UrlConstraint(['message'=>'The URL entered is invalid'])
                 ]
             ])
             ->getForm();
@@ -35,10 +40,14 @@ class UrlsController extends AbstractController
 
         if($form->isSubmitted() && $form->isValid()){
             //valider les infos
-
             //on vérif si url entré a déja été raccourcie
+            $url = $urlRepository->findOneBy(['original'=> $form['original']->getData()]);
             //si oui
-            //on retourne la version preview
+            if($url){
+                //on retourne la version preview
+                return $this->render('urls/preview.html.twig',compact('url'));
+            }
+
 
             //si l'url n'a pas déja été raccourcie
         }
@@ -46,5 +55,15 @@ class UrlsController extends AbstractController
         return $this->render('urls/create.html.twig', [
             'form' => $form->createView()
         ]);
+    }
+
+    /**
+     * @Route ("/{shortened}",name="app_urls_show")
+     *
+     * @param Url $url
+     * @return Response
+     */
+    public function show(Url $url):Response{
+        return $this->redirect($url->getOriginal());
     }
 }
