de0379dc91d0e923d0f9afff3ce659aefb06e81c
Aurelien <linux.aurelien@gmail.com>
2020-10-12 10:44:58 +0200

    Limit length to 6 for shortened field

--- migrations/Version20201012084155.php
+++ migrations/Version20201012084155.php
@@ -10,17 +10,17 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20201011204543 extends AbstractMigration
+final class Version20201012084155 extends AbstractMigration
 {
     public function getDescription() : string
     {
-        return 'Create urls table';
+        return 'Table_Urls';
     }
 
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE urls (id INT AUTO_INCREMENT NOT NULL, original VARCHAR(255) NOT NULL, shortened VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_2A9437A12F727085 (original), UNIQUE INDEX UNIQ_2A9437A178B5DC1 (shortened), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE urls (id INT AUTO_INCREMENT NOT NULL, original VARCHAR(255) NOT NULL, shortened VARCHAR(6) NOT NULL, UNIQUE INDEX UNIQ_2A9437A12F727085 (original), UNIQUE INDEX UNIQ_2A9437A178B5DC1 (shortened), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema) : void
